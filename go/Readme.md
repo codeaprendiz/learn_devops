23c762f698677de617df14a2b565ae66dd3f7c9b
codeaprendiz <codeaprendiz@gmail.com>
2021-02-23 22:31:11 +0400

    chore: update

--- go/Readme.md
+++ go/Readme.md
@@ -5,3 +5,4 @@
 
 - [hello-world](task-001-hello-world)
 - [variables](task-002-variables)
+- [operators](task-003-operators)
---
+++ go/task-003-operators/operators.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+   // Arithmetic Operators
+
+   fmt.Printf("10 + 2 =  %v\n", 10 + 2)
+   fmt.Printf("10 - 2 =  %v\n", 10 - 2)
+   fmt.Printf("10 / 2 =  %v\n", 10 / 2)
+   fmt.Printf("10 - 2 =  %v\n", 10 - 2)
+
+}
