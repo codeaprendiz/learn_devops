f0822c25f9afa62e96a58bdf5cc5e0783772c204
codeaprendiz <codeaprendiz@gmail.com>
2021-02-25 22:39:32 +0400

    chore: update

--- coding-tasks/task-005-nodejs-mongo-k8s-helm-scale/node_project/db.js
+++ coding-tasks/task-005-nodejs-mongo-k8s-helm-scale/node_project/db.js
@@ -16,6 +16,10 @@ const options = {
   connectTimeoutMS: 10000,
 };
 
+// Use the following URL when testing the application locally in docker-compose
+// const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+// default connection URI
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?replicaSet=${MONGO_REPLICASET}&authSource=admin`;
 
 mongoose.connect(url, options).then( function() {
