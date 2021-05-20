2e494c1fd2e95e1a1ffeb0fdde719c2a9df9e1fa
codeaprendiz <codeaprendiz@gmail.com>
2021-05-20 15:31:49 +0530

    chore: go update

--- go/task-006-maps/main.go
+++ go/task-006-maps/main.go
@@ -13,6 +13,29 @@ func main() {
 	delete(colors, "yellow")
 
 	printMap(colors)
+
+	m := map[string]int {
+		"James" : 1,
+		"Falcon" : 2,
+	}
+
+	v, ok := m["fakeKey"]
+	fmt.Println(v)
+	fmt.Println(ok)
+
+	if _, ok := m["James"]; ok {
+		fmt.Println("James key is present")
+	}
+
+	m["Todd"]=3
+
+	for k,v := range m {
+		fmt.Println(k,v)
+	}
+
+	delete(m,"Todd")
+
+	fmt.Println("Map after deleting Todd" , m)
 }
 
 func printMap(c map[string]string) {
