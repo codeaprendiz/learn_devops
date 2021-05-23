6735bdb56c49cc79ec32e88e66e4a3625214579c
codeaprendiz <codeaprendiz@gmail.com>
2021-05-23 22:11:12 +0530

    chore: go update

---
+++ go/task-042-waitgroup/ReadMe.md
@@ -0,0 +1,31 @@
+- Run the following 
+
+```bash
+$ go run main.go
+OS  darwin
+Arch  amd64
+CPUs  16
+Go routines   1
+bar  0
+bar  1
+bar  2
+bar  3
+bar  4
+bar  5
+bar  6
+bar  7
+bar  8
+bar  9
+CPUs  16
+Go routines   2
+foo  0
+foo  1
+foo  2
+foo  3
+foo  4
+foo  5
+foo  6
+foo  7
+foo  8
+foo  9
+```
--- go/task-042-waitgroup/main.go
+++ go/task-042-waitgroup/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	fmt.Println("OS ", runtime.GOOS)
@@ -11,4 +14,28 @@ func main() {
 	fmt.Println("CPUs ", runtime.NumCPU())
 	fmt.Println("Go routines  ", runtime.NumGoroutine())
 
+	wg.Add(1)
+
+	go foo()
+
+	bar()
+
+	fmt.Println("CPUs ", runtime.NumCPU())
+	fmt.Println("Go routines  ", runtime.NumGoroutine())
+
+	wg.Wait()
+}
+
+func foo() {
+	for i:=0; i<10; i++ {
+		fmt.Println("foo ",  i)
+	}
+
+	wg.Done()
+}
+
+func bar() {
+	for i:=0; i<10; i++ {
+		fmt.Println("bar ",i)
+	}
 }
