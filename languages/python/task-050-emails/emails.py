21a468555e1d1456334312a31dbd31e4bae207c4
codeaprendiz <codeaprendiz@gmail.com>
2021-06-11 09:34:03 +0530

    chore: python update

--- languages/python/task-050-emails/emails.py
+++ languages/python/task-050-emails/emails.py
@@ -10,6 +10,17 @@ from email.mime.text import MIMEText
 """
 CONSTANT_URL_REGEX = r"\b((?:https?://)?(?:(?:www\.)?(?:[\da-z\.-]+)\.(?:[a-z]{2,6})|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(?:(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])))(?::[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?(?:/[\w\.-]*)*/?)\b"
 
+"""
+########################################################################################################################################################
+# SMTP Related Constants required for automated emails
+# Elastic Emails - https://help.elasticemail.com/en/articles/2446055-what-are-sending-limits-for-new-account
+########################################################################################################################################################
+"""
+
+CONSTANT_SMTP_HOST = "smtp.somedomain.com"
+CONSTANT_SMTP_PORT = "2525"
+CONSTANT_SMTP_USERNAME = "ankit.rathi@mycompany.com"
+CONSTANT_SMTP_PASSWORD = "somepassword"
 
 class Email(object):
     """
@@ -219,3 +230,6 @@ class Email(object):
             print("Successfully sent email")
         except smtplib.SMTPException:
             print("Error: unable to send email" + str(smtplib.SMTPException))
+
+
+email_object = Email(CONSTANT_SMTP_HOST, CONSTANT_SMTP_PORT, CONSTANT_SMTP_USERNAME, CONSTANT_SMTP_PASSWORD)
