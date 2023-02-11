75eda8b6e402ef5f4fe498c0765bc41201bc3ee6
codeaprendiz <codeaprendiz@gmail.com>
2023-02-11 19:44:55 +0800

    chore: golang task-001 hello-world

---
+++ programming-languages/go/task-001-hello-world/go.mod
@@ -0,0 +1,3 @@
+module example/hello
+
+go 1.19
---
+++ programming-languages/go/task-001-hello-world/hello-world.go
@@ -0,0 +1,11 @@
+/* 
+Docs : https://go.dev/doc/tutorial/getting-started
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
