b03f25d7a500be9fcf294cace06e2b91930ed5be
codeaprendiz <codeaprendiz@gmail.com>
2023-02-15 22:41:43 +0800

    chore: golang task-019 methods

--- home/programming-languages/go/task-019-methods/methods.go
+++ home/programming-languages/go/task-019-methods/methods.go
@@ -10,11 +10,21 @@ func func1() {
 	fmt.Println("I am func1")
 }
 
+// A method is a function with a special receiver argument.
+// The receiver appears in its own argument list between the func keyword and the method name.
+// In this example, the Abs method has a receiver of type Vertex named v.
 func (v1 vertex) abs() int {
 	return v1.x * v1.y
+}
+
+// Remember: a method is just a function with a receiver argument.
+// Here's Abs written as a regular function with no change in functionality.
+func abs(v2 vertex) int {
+	return v2.x * v2.y
 }
 
 func main() {
 	v := vertex{2, 3}
 	fmt.Println(v.abs())
+	fmt.Println(abs(v))
 }
