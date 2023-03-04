6a396e5ad60b756ca590a0dd4001a2f4a8866c63
codeaprendiz <codeaprendiz@gmail.com>
2023-03-04 08:27:26 +0530

    chore: golang task-032 channels updates

--- home/programming-languages/go/task-032-channels-select/select.go
+++ home/programming-languages/go/task-032-channels-select/select.go
@@ -5,6 +5,8 @@ import "fmt"
 func fibonacci(c, quit chan int) {
 	x, y := 0, 1
 	for {
+		// The select statement lets a goroutine wait on multiple communication operations.
+		//A select blocks until one of its cases can run, then it executes that case. It chooses one at random if multiple are ready.
 		select {
 		case c <- x:
 			x, y = y, x+y
