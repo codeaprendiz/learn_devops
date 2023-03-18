1f4a5043a9bc007d669a50feb59c7acfced6ef87
codeaprendiz <codeaprendiz@gmail.com>
2023-03-18 17:41:36 +0400

    update

---
+++ home/programming-languages/go/task-034-server/projects/httpserver/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
