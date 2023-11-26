aaae6201271b7872a841608f32277942d4e67732
John Doe <john@doe.org>
2023-11-26 23:53:02 +0200

    example go-git commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"time"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+)
+
+func check_error(e error) {
+	if e != nil {
+		log.Println(e)
+	}
+}
+
+func main() {
+
+	// Get the current repo directory
+	git_dir, err := os.Getwd()
+	check_error(err)
+
+	// Opens an already existing repository
+	r, err := git.PlainOpen(git_dir)
+	check_error(err)
+
+	w, err := r.Worktree()
+	check_error(err)
+
+	// Adds the new file to the staging area
+	_, err = w.Add(".")
+	check_error(err)
+
+	// We can verify the current status of the worktree using the method Status
+	status, err := w.Status()
+	check_error(err)
+	fmt.Println(status)
+
+	// Commits the current staging area to the repository
+	commit, err := w.Commit("example go-git commit", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "John Doe",
+			Email: "john@doe.org",
+			When:  time.Now(),
+		},
+	})
+	check_error(err)
+
+	// Prints the current HEAD to verify that all worked well
+	obj, err := r.CommitObject(commit)
+	check_error(err)
+
+	fmt.Println(obj)
+
+	// Push using default options
+	err = r.Push(&git.PushOptions{})
+	check_error(err)
+
+}
